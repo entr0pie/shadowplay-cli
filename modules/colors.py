e81840d96c581dc117df7032d9178e4f639fd2e5
Caio Porcel <caiohporcel@gmail.com>
2022-12-28 12:37:45 -0300

    Added colors to shadowplay.py

--- modules/colors.py
+++ modules/colors.py
@@ -2,8 +2,13 @@
 
 class Colors: 
     def __init__(self):
-        self.ok = f"\u001b[32;1m"
-        self.fail = f"\u001b[31;1m"
-        self.warn = f"\u001b[33;1m"
-        self.bold = f"\u001b[37;1m"
-        self.reset = f"\u001b[0m"
+        self.ok = "\u001b[32;1m"
+        self.fail = "\u001b[31;1m"
+        self.warn = "\u001b[33;1m"
+        self.bold = "\u001b[37;1m"
+        self.blue = "\u001b[34;1m"
+        self.magenta = "\u001b[35;1m"
+        self.black = "\u001b[30;1m"
+        self.back_white = "\u001b[47m"
+        self.reset = "\u001b[0m"
+    
